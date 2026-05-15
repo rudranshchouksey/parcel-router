497d04582d389d1bcda4e7a40786735e722f39c4
Rudransh Chouksey <choukseyrudransh22@gmail.com>
2026-05-15 21:46:04 +0530

    Some error removed

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,5 +1,5 @@
 import { PrismaPg } from '@prisma/adapter-pg';
-import { PrismaClient } from '../generated/prisma/client';
+import { PrismaClient } from '@/generated/prisma/client';
 
 // Prevent multiple instances in Next.js dev hot-reloading
 const globalForPrisma = globalThis as unknown as {
